82859f20055f1b06adc7e6ea9f1f8db084e141c1
Lewis Daly <lewisdaly@me.com>
2018-10-29 13:08:39 +1030

    Add support for POST

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,38 @@ const request = require('request-promise');
 admin.initializeApp(functions.config().firebase);
 
 
-const callUrl = (url) => {
+const callUrl = (url, method, bodyStr) => {
+  if (method === 'GET') {
+    return callUrlGet(url);
+  }
+
+  if (method !== 'POST') {
+    throw new Error(`Unsupported Method: ${method}`);
+  }
+
+  return callUrlPost(url, method, bodyStr)
+}
+
+const callUrlPost = (url, method, body) => {
+  const options = {
+    method: 'POST',
+    uri: url,
+    json: true,
+    body,
+  };
+
+  return request(options)
+    .then(response => {
+      console.log(response);
+      return true;
+    })
+    .catch(err => {
+      console.error(err);
+      return false;
+    });
+}
+
+const callUrlGet = (url) => {
   const options = {
     method: 'GET',
     uri: url,
@@ -32,10 +63,15 @@ const everyMinuteHandler = (event) => {
   .then(snapshot => snapshot.val())
   .then(values => {
     return Promise.all(
-      Object.keys(values).map(key => callUrl(values[key]))
+      Object.keys(values).map(key => {
+        const obj = values[key];
+        return callUrl(obj.url, obj.method, obj.body);
+      })
     );
   });
 
 };
 
 exports.minute_job = functions.pubsub.topic('every-minute-tick').onPublish(everyMinuteHandler);
+
+exports.test = everyMinuteHandler;
